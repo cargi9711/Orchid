bb97cacba6619733ce24e662e980b19b104cde20
Bletokz <39291212+cargi9711@users.noreply.github.com>
2026-04-18 18:39:42 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,5 @@
+def growth_rate(temperature, humidity):
+    return (temperature * 0.3) + (humidity * 0.2)
+
+def describe_orchid(name):
+    return f"{name} is a delicate and beautiful flowering plant."
